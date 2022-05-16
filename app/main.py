07b2f9c059c065c9f00cdf6a5113200168de69d2
minjee jang <jmj3047@hufs.ac.kr>
2022-05-16 12:48:37 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -5,3 +5,25 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "hello world!"
+
+# 카카오톡 텍스트형 응답
+@app.route('/api/sayHello', methods=['POST'])
+def sayHello():
+    body = request.get_json() # 사용자가 입력한 데이터
+    print(body)
+    print(body['userRequest']['utterance'])
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "안녕 hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody

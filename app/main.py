93abc18cd51e9a73108ce541cd175a5bb8853c46
minjee jang <jmj3047@hufs.ac.kr>
2022-05-17 11:59:59 +0900

    updated

--- app/main.py
+++ app/main.py
@@ -1,6 +1,48 @@
 from flask import Flask, request
+import json
 
 app = Flask(__name__)
+
+##메인 로직
+def cals(opt_operator, number01, number02):
+    if opt_operator == "addition":
+        return number01 + number02
+    elif opt_operator == "subtraction": 
+        return number01 - number02
+    elif opt_operator == "multiplication":
+        return number01 * number02
+    elif opt_operator == "division":
+        return number01 / number02
+
+@app.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+    opt_operator = params_df['operators']
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
 
 @app.route('/')
 def hello_world():
